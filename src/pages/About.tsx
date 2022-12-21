5677b54e21792ef6dabc961db64ff268419f71f5
asyraf <asyraf@excelfixer.com>
2022-12-21 09:57:46 +0800

    add home and about

---
+++ src/pages/About.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    <div className="about">
+        <h1>About Us</h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure nesciunt voluptas impedit repellendus magnam dolores hic ipsam. Adipisci minima veniam distinctio, cum eveniet atque maiores? Rerum numquam error quod corrupti, incidunt vero ducimus nisi earum officiis minima enim quaerat possimus praesentium dolore deserunt commodi molestias hic perferendis nemo! Impedit earum enim possimus consequatur deleniti explicabo cumque? Impedit exercitationem facilis debitis excepturi, error cum esse laboriosam beatae optio, aspernatur natus! Provident cumque, neque impedit dolor voluptatum vero esse, voluptas fuga suscipit ullam perspiciatis quod voluptatem, repudiandae earum qui culpa. Amet tempora eius iusto veniam. Optio excepturi cum suscipit, vel ipsam molestiae?</p>
+    </div>
+    </>
+  )
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div className="home">
+        <h1>Home</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iusto culpa aliquid eaque at illo saepe quia ducimus, modi dolorem consequatur maiores possimus vel magnam dignissimos! Reprehenderit, facere. Ea eum vero sapiente ullam, ducimus minus qui fuga provident nam atque, incidunt obcaecati harum non quo molestiae assumenda. Exercitationem eius ad illum animi, quibusdam et? Earum excepturi quam, eligendi quas corporis soluta minus odit sequi rerum. Ullam deleniti porro expedita, fugiat aut mollitia reprehenderit natus sint possimus quisquam officia nihil est? Excepturi saepe nisi laudantium voluptates quis sapiente, sit voluptatem ab dignissimos, hic corporis provident modi, ea inventore voluptatibus impedit cupiditate tenetur nesciunt eius accusamus officiis labore. Dolor et ducimus, corrupti cupiditate nobis labore enim odit fugiat, hic temporibus eaque quaerat, ad provident suscipit repellendus deleniti pariatur eos dolore quidem delectus blanditiis. Nulla repudiandae ducimus, facere sed rerum laboriosam fugiat laudantium saepe sapiente harum iure tempore cum! Nostrum sequi voluptatem velit sed rem repudiandae, adipisci fuga iure atque, harum quis explicabo voluptas impedit rerum quas recusandae illo ipsa quos modi similique ipsum sunt? Aperiam provident aliquam, facilis eligendi quos atque aliquid similique deleniti. Eligendi ipsum quidem rem quia quod omnis. Mollitia vel deserunt nulla cumque tenetur ipsum distinctio. Minus, nesciunt iusto!</p>
+    </div>
+    </>
+  )
+}
+
+export default Home
